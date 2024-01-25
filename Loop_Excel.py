26fb7b4becd57d2abec66992991901ae1f1731d9
lucasg1729 <lucasg1729@gmail.com>
2024-01-25 15:00:47 -0500

    Currently loops through all of SV Health and its data is in the excel - still running into ad popup occasionally on first page that I will try to address
    took roughly 4 minutes to go through 34 files

--- Loop_Excel.py
+++ Loop_Excel.py
@@ -22,13 +22,14 @@ while search_check:
     time.sleep(2) #waits 2 seconds so that the autosuggest has enough time
     searcher.send_keys(Keys.DOWN)
     searcher.send_keys(Keys.ENTER)
-    time.sleep(2)
+    time.sleep(.5)
     if 'search' not in driver.current_url:
         search_check = False
 
 parent_window = driver.current_window_handle #gets the location ID to switch back to initial tab
 
 ### Gets name of firm
+time.sleep(.5)
 search_name = driver.find_element(By.XPATH, '/html/body/main/div[2]/div[1]/h3/span').text
 
 ### Opens converter and switches control back to main page
@@ -41,7 +42,6 @@ driver.switch_to.window(driver.window_handles[0])
 box = driver.find_element(By.XPATH, '//*[@id="searchbox"]') #finds the searchbox to filter types of documents
 box.click()
 box.send_keys('13F')
-time.sleep(2)
 full_number_text = driver.find_element(By.XPATH, '/html/body/main/div[5]/div/div[3]/div[4]').text[18:26]
 num_entries = ''
 for i in range(len(full_number_text)):
@@ -54,26 +54,27 @@ while row<=num_entries:
     next.click()
 
     ### Switches control to new tab
-    time.sleep(1)
+    time.sleep(.5)
     driver.switch_to.window(driver.window_handles[2])
     year = driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/div[2]/div[1]/div[2]').text[0:4]
     driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/table/tbody/tr[5]/td[3]/a').click()#opens xml file
-    time.sleep(1)
+    time.sleep(.5)
     xml = driver.find_element(By.CSS_SELECTOR, 'body').text #gets the text of the xml data
     index = xml.index('<')
     xml_data = xml[index:]
-    time.sleep(2)
+    driver.close()
 
     ###Switches control to xml to excel converter
-    driver.close()
     driver.switch_to.window(driver.window_handles[1])
+    if 'google' in driver.current_url:
+        driver.get('https://www.convertcsv.com/xml-to-csv.htm')
     conv_box = driver.find_element(By.XPATH, '//*[@id="txt1"]')
     driver.execute_script('arguments[0].scrollIntoView(true)', conv_box)
     conv_box.click()
+    conv_box.clear()
     conv_box.send_keys(xml_data) #inputs xml data
-    time.sleep(2)
+    time.sleep(.5)
     driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div[2]/input[2]').click() #downloads as excel
-    # make sure to clear data before next submission will do this soon
 
     ###Moves data from new excel to Master
     master_wb = xw.Book("/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx")
@@ -92,7 +93,7 @@ while row<=num_entries:
         newrow = 0
         temp_data = [i[:] for i in new_data_raw]
         for data_list in temp_data[1:]:
-            data_list.insert(0, 2000)
+            data_list.insert(0, year)
         temp_data[0].insert(0, 'Year')
     else:
         new_data_raw = newdata_wb.sheets[0].range('A2').expand().value
@@ -102,12 +103,11 @@ while row<=num_entries:
             data_list.insert(0, year)
     master_wb.sheets[search_name][newrow,0].value = temp_data
     master_wb.save("/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx")
-    time.sleep(2)
+    time.sleep(.5)
     newdata_wb.close()
     os.remove("/Users/lucasg17/Downloads/convertcsv.xlsx") #deletes file after used
 
     ###closing tabs and returning to first page
-    time.sleep(2)
     all_handles = driver.window_handles
     driver.switch_to.window(all_handles[0])
 
