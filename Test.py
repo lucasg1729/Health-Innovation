ece4e082cea73f574f41e55dc6ce11c48ea47bff
lucasg1729 <lucasg1729@gmail.com>
2024-03-03 11:54:06 -0500

    Cleaned up files
    Working on fixing conversion

--- Test.py
+++ Test.py
@@ -1,7 +1,9 @@
-from Execute_Function import execute, execute_all
+from Execute_Function_Updated import execute, execute_all
 
 ###Takes in the CIK ID, location of master excel on computer, location of where the first excel would be saved, location of where the second excel would be saved
-execute(ID='1587143', master_location="/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx", 
-        newdata_location="/Users/lucasg17/Downloads/convertcsv.xlsx", newdata_copy_location="/Users/lucasg17/Downloads/convertcsv (1).xlsx")
+# execute(ID='1587143', master_location="/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx", 
+#         newdata_location="/Users/lucasg17/Downloads/convertcsv.xlsx", newdata_copy_location="/Users/lucasg17/Downloads/convertcsv (1).xlsx")
 # execute_all(ID_list=['1231756', '1865991', '1587143', '1346824'], master_loc="/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx", 
 #         newdata_loc="/Users/lucasg17/Downloads/convertcsv.xlsx", newdata_copy_loc="/Users/lucasg17/Downloads/convertcsv (1).xlsx")
+execute(ID='1587143', master_location="/Users/lucasg17/Documents/GitHub/Health-Innovation/Master.xlsx", 
+        newdata_location="/Users/lucasg17/Downloads/result.xlsx", newdata_copy_location="/Users/lucasg17/Downloads/result (1).xlsx")
